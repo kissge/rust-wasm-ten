031f88221fe763e67d1cbbb2fc0661e3730c1f22
Yusuke Kido <mail@yusuk.eki.do>
2022-01-30 09:44:10 +0900

    FIXME

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 $ wasm-pack build
 $ cd www
 $ npm install
+$ export NODE_OPTIONS=--openssl-legacy-provider # if node > 16 or something
 $ npm start
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,14 @@ pub enum Operation {
 }
 
 #[wasm_bindgen]
+#[repr(i32)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum FormulaShape {
+    A, // ((a _ b) _ c) _ d
+    B, // ((a _b) _ (c _ d))
+}
+
+#[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
@@ -38,7 +46,6 @@ pub fn enumerate(a: i32, b: i32, c: i32, d: i32) -> Vec<js_sys::Array> {
         Operation::Div,
         Operation::DivInversed,
     ];
-    let operations = [&operations[..], &operations[..], &operations[..]].concat();
 
     [a, b, c, d]
         .iter()
@@ -47,60 +54,91 @@ pub fn enumerate(a: i32, b: i32, c: i32, d: i32) -> Vec<js_sys::Array> {
         .flat_map(|args| {
             operations
                 .iter()
+                .cycle()
+                .take(3 * operations.len())
                 .combinations_with_replacement(3)
                 .unique()
-                .filter_map(move |ops| {
-                    if calculate(&args, &ops) == Ok(10.0) {
-                        Some(get_array(&args, &ops))
-                    } else {
-                        None
-                    }
+                .flat_map(|ops| {
+                    [FormulaShape::A, FormulaShape::B]
+                        .iter()
+                        .filter_map(|shape| {
+                            if calculate(&args, &ops, &shape) == Ok(10.0) {
+                                Some(get_array(&args, &ops, &shape))
+                            } else {
+                                None
+                            }
+                        })
                 })
         })
         .collect()
 }
 
-pub fn calculate(args: &Vec<&i32>, ops: &Vec<&Operation>) -> Result<f32, ()> {
-    let mut result = Rational32::from_integer(*args[0]);
-
-    for (&arg, op) in args[1..].iter().zip(ops.iter()) {
+pub fn calculate_term(
+    a: Option<Rational32>,
+    b: Option<Rational32>,
+    op: Operation,
+) -> Option<Rational32> {
+    if let (Some(a), Some(b)) = (a, b) {
         match op {
-            Operation::Add => result += arg,
-            Operation::Sub => result -= arg,
-            Operation::SubInversed => result = Rational32::from_integer(*arg) - result,
-            Operation::Mul => result *= arg,
+            Operation::Add => Some(a + b),
+            Operation::Sub => Some(a - b),
+            Operation::SubInversed => Some(b - a),
+            Operation::Mul => Some(a * b),
             Operation::Div => {
-                if *arg == 0 {
-                    return Err(());
+                if b == Rational32::from_integer(0) {
+                    Some(a / b)
                 } else {
-                    result /= arg
+                    None
                 }
             }
             Operation::DivInversed => {
-                if result == Rational32::from_integer(0) {
-                    return Err(());
+                if a == Rational32::from_integer(0) {
+                    Some(b / a)
                 } else {
-                    result = Rational32::from_integer(*arg) / result
+                    None
                 }
             }
         }
+    } else {
+        None
     }
-
-    // if *ops[0] == Operation::Div {
-    //     log(&format!("{:?} {:?} {}", args, ops, result));
-    // }
-
-    Ok((*result.numer() as f32) / (*result.denom() as f32))
 }
 
-pub fn get_array(args: &Vec<&i32>, ops: &Vec<&Operation>) -> js_sys::Array {
-    let array = js_sys::Array::new_with_length(7);
+pub fn calculate(args: &Vec<&i32>, ops: &Vec<&Operation>, shape: &FormulaShape) -> Result<f32, ()> {
+    let args: Vec<Option<Rational32>> = args
+        .iter()
+        .map(|x| Some(Rational32::from_integer(**x)))
+        .collect();
+
+    let answer = match shape {
+        FormulaShape::A => {
+            let t = calculate_term(args[0], args[1], *ops[0]);
+            let t = calculate_term(t, args[2], *ops[1]);
+            calculate_term(t, args[3], *ops[2])
+        }
+        FormulaShape::B => {
+            let t1 = calculate_term(args[0], args[1], *ops[0]);
+            let t2 = calculate_term(args[2], args[3], *ops[1]);
+            calculate_term(t1, t2, *ops[2])
+        }
+    };
+
+    if let Some(answer) = answer {
+        Ok((*answer.numer() as f32) / (*answer.denom() as f32))
+    } else {
+        Err(())
+    }
+}
+
+pub fn get_array(args: &Vec<&i32>, ops: &Vec<&Operation>, shape: &FormulaShape) -> js_sys::Array {
+    let array = js_sys::Array::new_with_length(8);
     args.iter().enumerate().for_each(|(i, &arg)| {
         array.set(i as u32, JsValue::from(*arg));
     });
     ops.iter().enumerate().for_each(|(i, &op)| {
         array.set(i as u32 + 4, JsValue::from(*op as i32));
     });
+    array.set(7, JsValue::from(*shape as i32));
 
     array
 }
